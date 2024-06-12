26c0f3a546a0d00b0660826e221b446b95690922
fadhilalpr24 <fadhilahalyaprastinanda24@gmail.com>
2024-06-12 15:31:17 +0700

    brisol fam & iem

--- app/Imports/Brisol/FoundationFAMImport.php
+++ app/Imports/Brisol/FoundationFAMImport.php
@@ -3,60 +3,40 @@
 namespace App\Imports\Brisol;
 
 use App\Models\Brisol\FoundationFAM;
-use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\Importable;
-use Illuminate\Support\Collection;
 
-class FoundationFAMImport implements ToCollection, WithHeadingRow
+class FoundationFAMImport implements ToModel, WithHeadingRow
 {
-    use Importable;
-
-    public function collection(Collection $rows)
+    public function model(array $row)
     {
         ini_set('max_execution_time', 3600);
-        foreach ($rows as $row) {
-            $rowArray = $row->toArray();
-
-            $fam = FoundationFAM::where('buss_service_fam', $rowArray['business_service_fam'])->first();
-
-            if ($fam) {
-                $updatedFields = $this->checkForUpdates($fam, $rowArray);
-                if (!empty($updatedFields)) {
-                    $fam->update($updatedFields);
-                }
-            } else {
-                FoundationFAM::create($this->modelArray($rowArray));
+        
+        return DB::transaction(function () use ($row) {
+            // Definisikan kolom-kolom unik untuk pengecekan duplikasi
+            $uniqueFields = [
+                'buss_service_fam' => $row['business_service_fam'],
+                'prd_tier1' => $row['product_categorization_tier_1'],
+                'prd_tier2' => $row['product_categorization_tier_2'],
+                'prd_tier3' => $row['product_categorization_tier_3'],
+                'op_tier1' => $row['operational_categorization_tier_1'],
+                'op_tier2' => $row['operational_categorization_tier_2'],
+                'op_tier3' => $row['operational_categorization_tier_3'],
+                'resolution_category' => $row['resolution_categorization']
+            ];
+            
+            // Cek apakah data dengan kolom-kolom unik tersebut sudah ada di database
+            $existingRecord = FoundationFAM::where($uniqueFields)->first();
+            
+            if ($existingRecord) {
+                // Jika ditemukan duplikasi, lewati penyisipan data baru
+                return null;
             }
-        }
-    }
-
-    private function modelArray(array $row)
-    {
-        return [
-            'buss_service_fam' => $row['business_service_fam'],
-            'prd_tier1' => $row['product_categorization_tier_1'],
-            'prd_tier2' => $row['product_categorization_tier_2'],
-            'prd_tier3' => $row['product_categorization_tier_3'],
-            'op_tier1' => $row['operational_categorization_tier_1'],
-            'op_tier2' => $row['operational_categorization_tier_2'],
-            'op_tier3' => $row['operational_categorization_tier_3'],
-            'resolution_category' => $row['resolution_categorization']
-        ];
-    }
-
-    private function checkForUpdates(FoundationFAM $fam, array $row)
-    {
-        $attributes = $this->modelArray($row);
-        $changes = [];
-
-        foreach ($attributes as $key => $value) {
-            if ($fam->$key != $value) {
-                $changes[$key] = $value;
-            }
-        }
-
-        return $changes;
+            
+            // Jika tidak ditemukan duplikasi, buat data baru
+            return new FoundationFAM($uniqueFields);
+        });
     }
 
     public function headingRow(): int
--- app/Imports/Brisol/FoundationIEMImport.php
+++ app/Imports/Brisol/FoundationIEMImport.php
@@ -3,60 +3,40 @@
 namespace App\Imports\Brisol;
 
 use App\Models\Brisol\FoundationIEM;
-use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\Importable;
-use Illuminate\Support\Collection;
 
-class FoundationIEMImport implements ToCollection, WithHeadingRow
+class FoundationIEMImport implements ToModel, WithHeadingRow
 {
-    use Importable;
-
-    public function collection(Collection $rows)
+    public function model(array $row)
     {
         ini_set('max_execution_time', 3600);
-        foreach ($rows as $row) {
-            $rowArray = $row->toArray();
-
-            $iem = FoundationIEM::where('buss_service_iem', $rowArray['business_service_iem'])->first();
-
-            if ($iem) {
-                $updatedFields = $this->checkForUpdates($iem, $rowArray);
-                if (!empty($updatedFields)) {
-                    $iem->update($updatedFields);
-                }
-            } else {
-                FoundationIEM::create($this->modelArray($rowArray));
+        
+        return DB::transaction(function () use ($row) {
+            // Definisikan kolom-kolom unik untuk pengecekan duplikasi
+            $uniqueFields = [
+                'buss_service_iem' => $row['business_service_iem'],
+                'prd_tier1' => $row['product_categorization_tier_1'],
+                'prd_tier2' => $row['product_categorization_tier_2'],
+                'prd_tier3' => $row['product_categorization_tier_3'],
+                'op_tier1' => $row['operational_categorization_tier_1'],
+                'op_tier2' => $row['operational_categorization_tier_2'],
+                'op_tier3' => $row['operational_categorization_tier_3'],
+                'resolution_category' => $row['resolution_categorization']
+            ];
+            
+            // Cek apakah data dengan kolom-kolom unik tersebut sudah ada di database
+            $existingRecord = FoundationIEM::where($uniqueFields)->first();
+            
+            if ($existingRecord) {
+                // Jika ditemukan duplikasi, lewati penyisipan data baru
+                return null;
             }
-        }
-    }
-
-    private function modelArray(array $row)
-    {
-        return [
-            'buss_service_iem' => $row['business_service_iem'],
-            'prd_tier1' => $row['product_categorization_tier_1'],
-            'prd_tier2' => $row['product_categorization_tier_2'],
-            'prd_tier3' => $row['product_categorization_tier_3'],
-            'op_tier1' => $row['operational_categorization_tier_1'],
-            'op_tier2' => $row['operational_categorization_tier_2'],
-            'op_tier3' => $row['operational_categorization_tier_3'],
-            'resolution_category' => $row['resolution_categorization']
-        ];
-    }
-
-    private function checkForUpdates(FoundationIEM $iem, array $row)
-    {
-        $attributes = $this->modelArray($row);
-        $changes = [];
-
-        foreach ($attributes as $key => $value) {
-            if ($iem->$key != $value) {
-                $changes[$key] = $value;
-            }
-        }
-
-        return $changes;
+            
+            // Jika tidak ditemukan duplikasi, buat data baru
+            return new FoundationIEM($uniqueFields);
+        });
     }
 
     public function headingRow(): int
